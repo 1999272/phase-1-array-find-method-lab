f3ca4cfb9bc950b50221659de869527d81701759
Mark_Kamau <mark.wainaina@student.moringaschool.com>
2025-03-20 18:30:19 +0300

    completed the functionality

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// code your solution here
+// Function to find the year the Denver Broncos won the Super Bowl
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+}
